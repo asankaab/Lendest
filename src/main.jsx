3048a8bf964fd241a55b7fd26f4b5c63391670c5
asanka <asanka.abewickrama@gmail.com>
2025-12-25 02:37:30 +0530

    fix: resolve import path issues and add Auth and Theme contexts

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
 import { ThemeProvider } from './contexts/ThemeProvider.jsx'
-=======
-import { ThemeProvider } from './context/ThemeProvider.jsx'
->>>>>>> 13af10e341f71a153942c64c6efba3ad96e05558
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
